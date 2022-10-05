1805e840fede1f50cc9ac704f97413102a8f1021
ChristianSama <christiansama1601@gmail.com>
2022-10-04 23:19:35 -0500

    Add nested create on QuestionSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['pk', 'text']
 
 class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True)
+
     class Meta:
         model = Question
-        fields = ['pk', 'text']
+        fields = ['pk', 'text', 'choices']
+    
+    def create(self, validated_data):
+        choices_data = validated_data.pop('choices')
+        question = Question.objects.create(**validated_data)
+        for choice in choices_data:
+            Choice.objects.create(question=question, **choice)
+        return question
