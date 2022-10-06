98df49ef1d220bb4d3b58febfe7a43bcd5a793ac
ChristianSama <christiansama1601@gmail.com>
2022-10-06 18:00:46 -0500

    add handleUpdate

--- question-app-fe/src/App.tsx
+++ question-app-fe/src/App.tsx
@@ -5,7 +5,7 @@ import QuestionList from './components/QuestionList';
 function App() {
   return (
     <>
-      <h1>Hola xd</h1>
+      <h1>Super Quiz app</h1>
       <QuestionList />
       <QuestionForm />
     </>
--- question-app-fe/src/components/QuestionList.tsx
+++ question-app-fe/src/components/QuestionList.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { ChangeEvent, useEffect, useReducer, useState } from "react";
 import { Question } from "../types";
 
 const QuestionList = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [updating, setUpdating] = useState<boolean>(false);
 
   useEffect(() => {
     fetch("http://localhost:8000/api/questions/")
@@ -21,17 +22,44 @@ const QuestionList = () => {
     setQuestions(newQuestions)
   }
 
+  const handleUpdate = async (Qindex: number) => {
+    await fetch(`http://localhost:8000/api/questions/${questions[Qindex].pk}/`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: questions[Qindex].text,
+        choices: questions[Qindex].choices
+      }),
+    })
+    setUpdating(prev => !prev) 
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>, Qindex: number, Cindex: number) => {
+    const data = [...questions]
+    data[Qindex].choices[Cindex]['text'] = event.target.value
+    setQuestions(data);
+  }
+
   return (
     <>
-      {questions.map((question) => (
+      {questions.map((question, Qindex) => (
         <div key={question.pk}>
-          <h2>{question.text}</h2>
+          {updating 
+          ? <input type="text" value={question.text} onChange={(event) => handleChange(event, Qindex, -1)}></input>
+          : <h2>{question.text}</h2>}
           <ol>
-            {question.choices.map(choice => (
-              <li key={choice.pk}>{choice.text}</li>  
-            ))}
+            {question.choices.map((choice, Cindex) => {
+              return updating
+                ? <input type="text" value={choice.text} onChange={(event) => handleChange(event, Qindex, Cindex)}/>
+                : <li key={choice.pk}>{choice.text}</li>
+            })}
           </ol>
           <button onClick={() => handleDelete(question.pk)}>DELETE</button>
+          <button onClick={() => handleUpdate(Qindex)}>
+            {updating ? 'SAVE' : 'UPDATE'}
+          </button>
         </div>
       ))}
     </>
